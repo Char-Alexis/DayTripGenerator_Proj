3ca90f960c07f2162bde6828e8cb42680ece3063
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-02 00:12:35 -0400

    Fixing further errors in code on lines 5 and 12

--- daytrip.js
+++ daytrip.js
@@ -1,28 +1,13 @@
 "user strict"
 //As a user, I want a destination to be randomly selected for my day trip.
 
-
-function generateRandomDestination (array){
-        let newDestination = ["Cebu", "Boracay", "Bohol"]
-        for (let i=0; i< newDestination.length; i ++){
-        newDestination [i];
-        
-
-    let newDestination = Math.floor(Math.random() * 3);
-            
+function generateRandomDestination() {
+    for (let i = 0; i < allDestinations; i++){
+    console.log[i]
     }
-    random = generateRandomDestination[Math.floor(Math.random()* newDestination.length)];
-
+    let allDestinations = ["Cebu", "Bohol", "Samar"]
+    let newDestinations = allDestinations [Math.floor(Math.random() * allDestinations.length)]
 }
 
+generateRandomDestination()
 
-
-console.log = (newDestination)
-
-
-
-
-
-
-
-//As a user, I want a restaurant to be randomly selected for my day trip.
