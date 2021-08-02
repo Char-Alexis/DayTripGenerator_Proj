c0841c3a747febe80b48c3b3f8e9a808f4d600f0
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-02 00:43:46 -0400

    Rewriting code: lines 4-23

--- daytrip.js
+++ daytrip.js
@@ -2,7 +2,7 @@
 //As a user, I want a destination to be randomly selected for my day trip.
 
 function generateRandomDestination() {
-    for (let i = 0; i < allDestinations; i++){
+    for (let i = 0; i < generateRandomDestination.length; i++){
     console.log[i]
     }
     let allDestinations = ["Cebu", "Bohol", "Samar"]
@@ -11,3 +11,28 @@ function generateRandomDestination() {
 
 generateRandomDestination()
 
+//As a user, I want a restaurant to be randomly selected for my day trip.
+function generateRandomRestaurant() {
+    for (let i = 0; i < generateRandomRestaurant.length; i++){
+    console.log[i];
+    }
+    let allRestaurants = ["Cafe", "Buffet", "Fast-food"]
+    let newRestaurants = allRestaurants [Math.floor(Math.random() * allRestaurants.length)]
+}
+
+generateRandomRestaurant()
+
+function randomTransportation  (choice){
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+let modeOfTransportation = ["Car", "Taxi", "Bike"];
+
+console.log(randomTransportation(modeOfTransportation));
+
+//As a user, I want to be able to confirm that my day trip is "complete"
+function completeTrip (){
+    return "Complete"
+}
+
+console.log("Complete")
