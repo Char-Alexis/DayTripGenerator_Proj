bbe220d85f9815f10aff884007caceae90b7cbc2
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-01 21:35:36 -0400

    Correct syntax errors

---
+++ daytrip.js
@@ -0,0 +1,28 @@
+"user strict"
+//As a user, I want a destination to be randomly selected for my day trip.
+
+
+function generateRandomDestination (array){
+        let newDestination = ["Cebu", "Boracay", "Bohol"]
+        for (let i=0; i< newDestination.length; i ++){
+        newDestination [i];
+        
+
+    let newDestination = Math.floor(Math.random() * 3);
+            
+    }
+    random = generateRandomDestination[Math.floor(Math.random()* newDestination.length)];
+
+}
+
+
+
+console.log = (newDestination)
+
+
+
+
+
+
+
+//As a user, I want a restaurant to be randomly selected for my day trip.
